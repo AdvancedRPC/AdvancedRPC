397002cfd741e7b327653ebe22fd5719edcea407
LPTP1Dev <lptp1hd@gmail.com>
2020-09-06 16:11:43 +0200

    added functionality, fixed bugs, set icon
    Signed-off-by: LPTP1Dev <lptp1hd@gmail.com>

--- main.js
+++ main.js
@@ -2,6 +2,20 @@ const electron = require('electron')
 const express = require("express")
 const websocket = express()
 const DiscordRPC = require("discord-rpc");
+const rpc = new DiscordRPC.Client({ transport: 'ipc' });
+var onState = false;
+var offState = true;
+var win;
+
+var presenceData = {
+  clientId: "623182972895494164",
+  largeIMG: "lptp1",
+  largeTEXT: "LPTP1",
+  smallIMG: "discord",
+  smallTEXT: "AdvancedRPC",
+  details: "AdvancedRPC",
+  state: "Programmiert am Backend"
+};
 
 const { menubar } = require('menubar');
 
@@ -9,7 +23,7 @@ const { app, BrowserWindow , Tray , Menu} = require('electron')
 
 function createWindow () {
   // Create the browser window.
-  const win = new BrowserWindow({
+  win = new BrowserWindow({
     width: 800,
     height: 600,
     frame: false,
@@ -26,14 +40,12 @@ function createWindow () {
 app.whenReady().then(() => {
     tray = new Tray('./icon.ico')
   const contextMenu = Menu.buildFromTemplate([
-    { label: 'AdvancedRPC', click: () => { createWindow; } },
-    { label: 'Item1', type: 'radio' },
-    { label: 'Item2', type: 'radio' },
-    { label: 'Item3', type: 'radio', checked: true },
-    { label: 'Item4', type: 'radio' },
+    { label: 'AdvancedRPC', click: () => { createWindow(); } },
+    { label: 'On', type: 'radio' , checked: onState, click: () => { onState=true; offState=false; startPresence(); }},
+    { label: 'Off', type: 'radio' , checked: offState, click: () => { onState=false; offState=true; stopPresence(); }},
     { label: 'Quit', click: () => { app.quit(); } }
   ])
-  tray.setToolTip('BetterRPC')
+  tray.setToolTip('AdvancedRPC')
   tray.setContextMenu(contextMenu)
 })
 
@@ -54,9 +66,6 @@ app.on('activate', () => {
 
 
 websocket.post("/update", async function (req, res) {
-
-    const rpc = new DiscordRPC.Client({ transport: 'ipc' });
-
     const clientId = req.body.clientid;
     const largeIMG = req.body.largeimg;
     const largeTEXT = req.body.largetext;
@@ -64,6 +73,15 @@ websocket.post("/update", async function (req, res) {
     const smallTEXT = req.body.smalltext;
     const state = req.body.state;
     const details = req.body.details;
+
+
+    presenceData.clientId = clientId;
+    presenceData.largeIMG = largeIMG;
+    presenceData.largeTEXT = largeTEXT;
+    presenceData.smallIMG = smallIMG;
+    presenceData.smallTEXT = smallTEXT;
+    presenceData.state = state;
+    presenceData.details = details;
 
     rpc.setActivity({
         details: details,
@@ -81,6 +99,53 @@ websocket.post("/update", async function (req, res) {
     res.redirect("file://index.html")
 })
 
+async function stopPresence() {
 
+}
+
+async function startPresence() {
+  if (!win) {
+    createWindow();
+    setTimeout(() => {
+      const { clientId, largeIMG, largeTEXT, smallIMG, smallTEXT, state, details } = presenceData;
+
+  rpc.setActivity({
+    details: details,
+    state: state,
+    largeImageKey: largeIMG,
+    largeImageText: largeTEXT,
+    smallImageKey: smallIMG,
+    smallImageText: smallTEXT,
+    instance: false,
+  });
+  try {
+    rpc.login({ clientId })
+  } catch(e) {
+      offState=true;
+      onState=false;
+      console.log(e)
+  }
+    }, 9000)
+  } else {
+    const { clientId, largeIMG, largeTEXT, smallIMG, smallTEXT, state, details } = presenceData;
+
+  rpc.setActivity({
+    details: details,
+    state: state,
+    largeImageKey: largeIMG,
+    largeImageText: largeTEXT,
+    smallImageKey: smallIMG,
+    smallImageText: smallTEXT,
+    instance: false,
+  });
+  try {
+    rpc.login({ clientId })
+  } catch(e) {
+      offState=true;
+      onState=false;
+      console.log(e)
+  }
+}
+}
 
 websocket.listen(4423, console.log("Running"))
